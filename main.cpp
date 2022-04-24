633dc4c718cd9c8c7aa79a9848b0b57bd7661460
Younes Bouddou <ybouddou@e1r7p10.1337.ma>
2022-04-24 22:27:44 +0000

    testing the AVL tree

--- main.cpp
+++ main.cpp
@@ -2,31 +2,37 @@
 #include <vector>
 #include <cmath>
 #include "vector.hpp"
+#include "tree.hpp"
+#include "BST.hpp"
 
 int main()
 {
-	std::vector<int> vec;
-	ft::vector<int> v;
+	BBST<std::string> mytree;
 
-	vec.push_back(1);
-	vec.push_back(2);
-	vec.push_back(3);
-
-	v.push_back(1);
-	v.push_back(2);
-	v.push_back(3);
-
-	std::vector<int>::iterator rit = vec.begin();
-	std::vector<int>::iterator myrit = vec.begin() + 2;
-	std::vector<int>::iterator::difference_type myrit2 = myrit - rit;
-	// ft::vector<int>::iterator rit = v.begin();
-	// ft::vector<int>::iterator myrit = v.begin() + 2;
-	// ft::vector<int>::iterator::difference_type myrit2 = myrit - rit;
-	// ft::vector<int>::reverse_iterator myrit2 = rit + 1;
-
-	// std::cout << *rit << "\n";
-	// std::cout << *myrit << "\n";
-	std::cout << myrit2 << "\n";
-	// std::cout << *rit << "\n";
-
+	mytree.insert("fjdskf");
+	mytree.insert("fsakdfs");
+	mytree.insert("reqwf");
+	mytree.insert("cvas");
+	mytree.insert("aafew");
+	mytree.insert("vasfd");
+	mytree.insert("ewr");
+	// mytree.insert(5);
+	// mytree.insert(6);
+	// mytree.insert(2);
+	// mytree.insert(4);
+	// mytree.insert(9);
+	// mytree.insert(7);
+	// mytree.insert(80);
+	// mytree.insert(10);
+	// mytree.insert(11);
+	// mytree.insert(12);
+	// mytree.insert(15);
+	// mytree.insert(20);
+	// mytree.insert(40);
+	// mytree.insert(29);
+	// mytree.insert(1);
+	// mytree.insert(3);
+	// mytree.insert(111);
+	// std::cout << mytree.find(12) << std::endl;
+	mytree.print();
 }
