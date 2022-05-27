c9b58395cd23b189ad664aa725b05222e3a9d997
Younes Bouddou <ybouddou@e1r7p15.1337.ma>
2022-05-27 11:32:24 +0100

    adding map and linking it with my avl tree
    adding all member functions and testing them with my tree
    still need more tests for some functions

--- map.hpp
+++ map.hpp
@@ -6,7 +6,7 @@
 /*   By: ybouddou <ybouddou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/17 23:32:01 by ybouddou          #+#    #+#             */
-/*   Updated: 2022/05/21 22:44:49 by ybouddou         ###   ########.fr       */
+/*   Updated: 2022/05/27 11:31:15 by ybouddou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <iostream>
 #include "reverseIterator.hpp"
 #include "pair.hpp"
-#include "binarySearchTree.hpp"
+#include "AVLTree.hpp"
 #include "map_iterator.hpp"
 
 namespace ft
@@ -45,19 +45,19 @@ namespace ft
 					}
 			};
 			
-			typedef Alloc											allocator_type;
-			typedef typename allocator_type::reference				reference;
-			typedef typename allocator_type::const_reference		const_reference;
-			typedef typename allocator_type::pointer				pointer;
-			typedef typename allocator_type::const_pointer			const_pointer;
-			typedef BBST<value_type, key_compare, allocator_type>	tree;
-			typedef typename tree::nodePtr							nodePtr;
-			typedef	ft::mapiterator<pointer, nodePtr>				iterator;
-			typedef	ft::mapiterator<const_pointer, nodePtr>			const_iterator;
-			typedef	ft::reverseiterator<iterator>					reverse_iterator;
-			typedef	ft::reverseiterator<iterator>					const_reverse_iterator;
-			typedef	typename allocator_type::difference_type 		difference_type;
-			typedef	typename allocator_type::size_type 				size_type;
+			typedef Alloc												allocator_type;
+			typedef typename allocator_type::reference					reference;
+			typedef typename allocator_type::const_reference			const_reference;
+			typedef typename allocator_type::pointer					pointer;
+			typedef typename allocator_type::const_pointer				const_pointer;
+			typedef AVLTree<value_type, key_compare, allocator_type>	tree;
+			typedef typename tree::nodePtr								nodePtr;
+			typedef	ft::mapiterator<pointer, nodePtr>					iterator;
+			typedef	ft::mapiterator<const_pointer, nodePtr>				const_iterator;
+			typedef	ft::reverseiterator<iterator>						reverse_iterator;
+			typedef	ft::reverseiterator<iterator>						const_reverse_iterator;
+			typedef	typename allocator_type::difference_type 			difference_type;
+			typedef	typename allocator_type::size_type 					size_type;
 		
 		//SECTION - PRIVATE DATA MEMBERS
 		private:
