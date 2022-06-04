9a9956e3ccc753e8d325a5aefe1450cd6bb9d081
Younes Bouddou <ybouddou@e1r7p15.1337.ma>
2022-06-04 19:14:42 +0100

    a lot of tests to test all containers
    test all containers with time and compare it with original containers

--- main.cpp
+++ main.cpp
@@ -6,10 +6,11 @@
 /*   By: ybouddou <ybouddou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 15:57:33 by ybouddou          #+#    #+#             */
-/*   Updated: 2022/05/27 21:13:33 by ybouddou         ###   ########.fr       */
+/*   Updated: 2022/06/04 19:12:51 by ybouddou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
 #include "vector.hpp"
 #include "stack.hpp"
 #include "map.hpp"
@@ -17,11 +18,115 @@
 #include <vector>
 #include <stack>
 #include <map>
-#include <iostream>
+#include <string>
+#include <deque>
+#include <stdlib.h>
 
 #ifndef NS
-#define NS ft
+#define NS std
 #endif
+
+// #define MAX_RAM 4294967296
+// #define BUFFER_SIZE 4096
+// struct Buffer
+// {
+// 	int idx;
+// 	char buff[BUFFER_SIZE];
+// };
+
+
+// #define COUNT (MAX_RAM / (int)sizeof(Buffer))
+
+// template<typename T>
+// class MutantStack : public NS::stack<T>
+// {
+// public:
+// 	MutantStack() {}
+// 	MutantStack(const MutantStack<T>& src) { *this = src; }
+// 	MutantStack<T>& operator=(const MutantStack<T>& rhs) 
+// 	{
+// 		this->c = rhs.c;
+// 		return *this;
+// 	}
+// 	~MutantStack() {}
+
+// 	typedef typename NS::stack<T>::container_type::iterator iterator;
+
+// 	iterator begin() { return this->c.begin(); }
+// 	iterator end() { return this->c.end(); }
+// };
+
+// int main(int argc, char** argv) {
+// 	if (argc != 2)
+// 	{
+// 		std::cerr << "Usage: ./test seed" << std::endl;
+// 		std::cerr << "Provide a seed please" << std::endl;
+// 		std::cerr << "Count value:" << COUNT << std::endl;
+// 		return 1;
+// 	}
+// 	const int seed = atoi(argv[1]);
+// 	srand(seed);
+
+// 	NS::vector<std::string> vector_str;
+// 	NS::vector<int> vector_int;
+// 	NS::stack<int> stack_int;
+// 	NS::vector<Buffer> vector_buffer;
+// 	NS::stack<Buffer, std::deque<Buffer> > stack_deq_buffer;
+// 	NS::map<int, int> map_int;
+
+// 	for (int i = 0; i < COUNT; i++)
+// 	{
+// 		vector_buffer.push_back(Buffer());
+// 	}
+
+// 	for (int i = 0; i < COUNT; i++)
+// 	{
+// 		const int idx = rand() % COUNT;
+// 		vector_buffer[idx].idx = 5;
+// 	}
+// 	NS::vector<Buffer>().swap(vector_buffer);
+
+// 	try
+// 	{
+// 		for (int i = 0; i < COUNT; i++)
+// 		{
+// 			const int idx = rand() % COUNT;
+// 			vector_buffer.at(idx);
+// 			std::cerr << "Error: THIS VECTOR SHOULD BE EMPTY!!" <<std::endl;
+// 		}
+// 	}
+// 	catch(const std::exception& e)
+// 	{
+// 		//NORMAL ! :P
+// 	}
+	
+// 	for (int i = 0; i < COUNT; ++i)
+// 	{
+// 		map_int.insert(NS::make_pair(rand(), rand()));
+// 	}
+
+// 	int sum = 0;
+// 	for (int i = 0; i < 10000; i++)
+// 	{
+// 		int access = rand();
+// 		sum += map_int[access];
+// 	}
+// 	std::cout << "should be constant with the same seed: " << sum << std::endl;
+
+// 	{
+// 		NS::map<int, int> copy = map_int;
+// 	}
+// 	MutantStack<char> iterable_stack;
+// 	for (char letter = 'a'; letter <= 'z'; letter++)
+// 		iterable_stack.push(letter);
+// 	for (MutantStack<char>::iterator it = iterable_stack.begin(); it != iterable_stack.end(); it++)
+// 	{
+// 		std::cout << *it;
+// 	}
+// 	std::cout << std::endl;
+// 	return (0);
+// }
+
 
 int main()
 {
@@ -1043,11 +1148,11 @@ int main()
         mymap.get_allocator().deallocate(p, 5);
 
         NS::map<int, std::string> m;
-        m.insert(NS::make_pair(1, "mojahid"));
-        m.insert(NS::make_pair(2, "yassir"));
-        m.insert(NS::make_pair(3, "zakaria"));
+        m.insert(NS::make_pair(1, "younes"));
+        m.insert(NS::make_pair(2, "choaib"));
+        m.insert(NS::make_pair(3, "abdelah"));
         NS::map<int, std::string>::iterator it = m.begin();
-        m[3] = "khaoula";
+        m[3] = "hatim";
         for (; it != m.end(); it++)
         {
             std::cout << it->first << "\t" << it->second << std::endl;
